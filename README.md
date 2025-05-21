d686d829284a9c53a373deccf1794b7aa60721fb
Vishnu Narayanan <iamwishnu@gmail.com>
2025-05-21 20:45:18 +0530

    feat: init android sdk 1.0.0
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ README.md
@@ -0,0 +1,34 @@
+# ChatwootSDK
+
+Android SDK for Chatwoot
+
+## Development Setup
+
+Before building or publishing, please run:
+
+```bash
+./gradlew wrapper --gradle-version 8.5
+```
+
+This will generate the required Gradle wrapper JAR file in the gradle/wrapper directory.
+
+## Usage in other projects
+
+Add the JitPack repository to your build file:
+
+```kotlin
+allprojects {
+    repositories {
+        ...
+        maven(url = "https://jitpack.io")
+    }
+}
+```
+
+Add the dependency:
+
+```kotlin
+dependencies {
+    implementation("com.github.chatwoot:chatwoot-sdk:1.0.0")
+}
+```
---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.chatwoot.sdk"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    tasks.withType<Jar> {
+        if (name.contains("releaseSourcesJar")) {
+            enabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("androidx.webkit:webkit:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("io.coil-kt:coil:2.5.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.findProperty("group") as String? ?: "com.github.chatwoot"
+                artifactId = "chatwoot-sdk"
+                version = project.findProperty("version") as String? ?: "1.0.12"
+
+                from(components["release"])
+            }
+        }
+    }
+}
